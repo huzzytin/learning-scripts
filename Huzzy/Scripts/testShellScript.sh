94a7f902b6c1e4bc91da892a6cfd029c0047d2e0
huzzytin <huzaifa.tin.edu@gmail.com>
2022-11-02 12:10:31 -0400

    Updating my repo with a new Scripts folder

---
+++ Huzzy/Scripts/testShellScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Started the script execution"
+
+cd /d/Pragra/learning-scripts/Huzzy
+
+cp *.sh ./Scripts/.
+
+echo "all script files are copied to main directory"
+
+cd /d/Pragra/learning-scripts
+
+echo "Present working directory is now local repo Home"
+
+Exit 0
